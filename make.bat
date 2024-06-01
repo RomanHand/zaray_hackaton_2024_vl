70536b44e2ac28533ed3ba7ad1bf1699740659d1
romanhand <roma25r@gmail.com>
2024-06-01 18:00:35 +1000

    update

--- make.bat
+++ make.bat
@@ -2,6 +2,6 @@ pip install -r .\cutter\requirements.txt
 pip install -r .\neiro\requirements.txt 
 pyinstaller .\cutter\cut.py --onefile --clean
 copy .\cutter\dist\cut C:/neiro/bin/cut
-pyinstaller .\neiro\detector.py --onefile --clean
+pyinstaller .\neiro\detector.py --onefile --noconsole --add-data=".\ultralytics\cfg\default.yaml:./ultralytics/cfg"
 copy .\cutter\dist\cut C:/neiro/bin/detector
-
+go build amnyam/cmd/amnyam/main.go -o C:/neiro/bin/amnyam
