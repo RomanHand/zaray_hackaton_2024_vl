05047321127fe81c19836fffa4816943476bf635
wideino <wideino@mail.ru>
2024-06-01 21:46:28 +1000

    some fixes, added previews writer

--- detector/detector.py
+++ detector/detector.py
@@ -1,10 +1,12 @@
 import cv2
 import sys
+import os
 import json
 from ultralytics import YOLO
 from math import floor
 from functools import reduce
-import time
+#import time
+import torch
 
 
 class Config:
@@ -13,13 +15,16 @@ class Config:
         - CONF_THRESHOLD: Порог вероятности фиксации нарушения
         - MIN_FRAME_AGE: Количество прошедших кадров для обнаружения нарушения
         - FPS_SPLIT: Количество кадров на 1 секунду видео
+        - MIN_INTERVAL_DISTANCE - Минимальное расстояние в секундах между 
+        интервалами, чтобы считать их разными
         - LIFE_TIME: Допустимый интервал утечки ошибки из кадра
     """
 
-    def __init__(self, conf_threshold=0.6, min_frame_age=3, fps_split=1.5, life_time=2):
+    def __init__(self, conf_threshold=0.5, min_frame_age=3, fps_split=3, life_time=2):
         self.CONF_THRESHOLD = conf_threshold
         self.MIN_FRAME_AGE = min_frame_age
         self.FPS_SPLIT = fps_split
+        self.MIN_INTERVAL_DISTANCE = 5
         self.LIFE_TIME = life_time
 
 
@@ -28,12 +33,21 @@ def glue_last_intervals(intervals: list):
     intervals.remove(intervals[-1])
 
 
-def process_file(path_source, model, config: Config):
+def write_previews(video_name, cap, fps, intervals):
+    os.makedirs('previews', exist_ok=True)
+    for i, interval in enumerate(intervals):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, interval[0] * fps)
+        _, frame = cap.read()
+        out_path = f"previews/{video_name}_{i}.png"
+        cv2.imwrite(out_path, frame)
+
+
+def process_file(video_name, path_source, model, config: Config):
     cap = cv2.VideoCapture(path_source)
 
     frame_age = 0
     frame_pos = 0
-    prev_detected = False
+    prev_detected = 0
 
     frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     fps = cap.get(cv2.CAP_PROP_FPS)
@@ -50,14 +64,14 @@ def process_file(path_source, model, config: Config):
         confs = reduce(lambda prev, cur: prev + cur.boxes.conf.detach().cpu().tolist(), results, [])
         if len(list(filter(lambda conf: conf > config.CONF_THRESHOLD, confs))) >= 1:
             frame_age += 1
-            prev_detected = True
-        elif frame_age >= config.MIN_FRAME_AGE and prev_detected:
+            prev_detected = frame_pos
+        elif frame_age >= config.MIN_FRAME_AGE and (frame_pos - prev_detected) / fps > config.LIFE_TIME:
             interval_end = (frame_pos - frames_increment) / fps
             intervals[-1].append(interval_end)
             prev_detected = False
             frame_age = 0
 
-            if len(intervals) > 1 and intervals[-1][0] - intervals[-2][1] <= config.LIFE_TIME:
+            if len(intervals) > 1 and intervals[-1][0] - intervals[-2][1] <= config.MIN_INTERVAL_DISTANCE:
                 glue_last_intervals(intervals)
 
         if frame_age >= config.MIN_FRAME_AGE and (len(intervals) == 0 or len(intervals[-1]) != 1):
@@ -66,6 +80,10 @@ def process_file(path_source, model, config: Config):
 
         frame_pos += frames_increment
 
+    if len(intervals) > 1 and len(intervals[-1]) == 1:
+        intervals[-1].append(frame_count / fps)
+
+    write_previews(video_name, cap, fps, intervals)
     cap.release()
 
     return intervals
@@ -76,7 +94,7 @@ def write_json(intervals: list, video_name, path_source):
 
     for i in range(len(intervals)):
         violation = dict()
-        violation["preview"] = f"{video_name}_{i}.jpeg"
+        violation["preview"] = f"previews/{video_name}_{i}.png"
         violation["start"] = round(intervals[i][0], 2)
         violation["end"] = round(intervals[i][1], 2)
         violations.append(violation)
@@ -95,12 +113,14 @@ def main(video_name, path_source, model_path, config_args=None):
             config_args[key] = float(config_args[key])
         config = Config(*config)
 
-    model = YOLO(model_path)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    #print('Using device:', device)
+    model = YOLO(model_path).to(device)
 
-    start_time = time.time()
-    intervals = process_file(path_source, model, config)
+    #start_time = time.time()
+    intervals = process_file(video_name, path_source, model, config)
     write_json(intervals, video_name, path_source)
-    print(f'spend time: {time.time() - start_time}')
+    #print(f'Spend time: {time.time() - start_time}')
 
 
 if __name__ == '__main__':
